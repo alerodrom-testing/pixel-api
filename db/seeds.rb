b064b400e99aeb6a3d6b9e2fde0b8ce780bb4b98
Alejandro <alerodri1994@gmail.com>
2020-11-30 18:22:24 +0100

    Fixed password encrypt.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,9 @@
 require 'faker'
 
 # Create user
-User.create(username: "alerodrom", password_digest: "123456789abc", age: 26)
+User.create(username: "alerodrom", password: "123456789abc", age: 26)
 
 # Create 10 Users with random username
 10.times do
-    user = User.create(username: Faker::Internet.username, password_digest: "123456789abc", age: 26)
+    user = User.create(username: Faker::Internet.username, password: "123456789abc", age: 26)
 end
