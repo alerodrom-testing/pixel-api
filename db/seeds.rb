27d0fe077292882817bf04d8dd48f648add36026
Alejandro <alerodri1994@gmail.com>
2020-11-30 19:36:10 +0100

    Added pixels into seed.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
 require 'faker'
 
 # Create user
-User.create(username: "alerodrom", password: "123456789abc", age: 26)
+base_user = User.create(username: "alerodrom", password: "123456789abc", age: 26)
+5.times do
+    Pixel.create(user: base_user, axis_x: rand(1..10), axis_y: rand(1..10))
+end
 
 # Create 10 Users with random username
 10.times do
     user = User.create(username: Faker::Internet.username, password: "123456789abc", age: 26)
+    5.times do
+        Pixel.create(user: user, axis_x: rand(1..10), axis_y: rand(1..10))
+    end
 end
